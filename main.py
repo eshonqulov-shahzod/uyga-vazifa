dc4ff0fe4660c67942918a4cb07443d3f7d580c2
eshonqulov-shahzod <eshonshahzod01@gmail.com>
2023-07-15 10:43:55 +0500

    14_07_23

--- main.py
+++ main.py
@@ -1,49 +1,86 @@
 """
 
-12.07.23 darsdagi uyga-vazifa
+14.07.23 darsdagi uyga-vazifa
 
-3 - masala bor
+4 ta masala bor
 
 """
 
-#   1 - misol
-
-################
-
-#   2 - misol
+#   1 - misol  ( 292 - misol )
 
 class Solution(object):
-    def isPowerOfThree(self, n):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 4 != 0:
+            return True
+        else:
+            return False
+
+
+#   2 - misol  ( 342 - misol)
+
+class Solution(object):
+    def isPowerOfFour(self, n):
         """
         :type n: int
         :rtype: bool
         """
 
-        list1 = []
-        for i in range(20):
-            if n == 3 ** i:
+        for i in range(16):
+            if pow(4, i) == n:
                 return True
         return False
 
 
-#   3 - misol
+
+#   3 - misol   (344 - misol)
 
 class Solution(object):
-    def countBits(self, n):
+    def reverseString(self, s):
         """
-        :type n: int
-        :rtype: List[int]
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
         """
-        list1 = [0]
-        for i in range(1, n + 1):
-            binary = ''
-            while i > 0:
-                binary = str(i % 2) + binary
-                i = i // 2
-            son = 0
-            for j in binary:
-                son += int(j)
-            list1.append(son)
-        return list1
+        n1 = 0
+        n2 = len(s) -1
 
+        while n1 < n2:
+            s[n1], s[n2] = s[n2], s[n1]
+            n1 += 1
+            n2 -= 1
+        return s
+
+
+
+#   4 - misol   (345 - misol)
+
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+
+        list1 = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        list2 = []
+
+        for i in range(len(s)):
+            if s[i] in list1:
+                list2.append(i)
+
+        list3 = list2[::-1]
+
+        for i in range(len(list2) // 2):
+            s[list2[i]], s[list3[i]] = s[list3[i]], s[list2[i]]
+
+        s1 = ""
+        for i in s:
+            s1 += i
+
+        return s1
 
