8e35b2f367f8e387468db6dff99c2331c03cd83c
eshonqulov-shahzod <eshonshahzod01@gmail.com>
2023-07-18 12:27:59 +0500

    17_07

--- main.py
+++ main.py
@@ -1,86 +1,112 @@
 """
 
-14.07.23 darsdagi uyga-vazifa
+17.07.23 darsdagi uyga-vazifa
 
 4 ta masala bor
 
 """
 
-#   1 - misol  ( 292 - misol )
+#   1 - misol  ( 367 - misol )
 
 class Solution(object):
-    def canWinNim(self, n):
+    def isPerfectSquare(self, num):
         """
-        :type n: int
+        :type num: int
         :rtype: bool
         """
-        if n % 4 != 0:
+        if num == 1:
             return True
-        else:
-            return False
+        if num < 17:
+            for i in range(1, num/2 + 1):
+                if i * i == num:
+                    return True
+        elif 17 < num <=100:
+            for i in range(4, num/4):
+                if i * i == num:
+                    return True
+        elif 100 < num <=1000:
+            for i in range(10, num/10):
+                if i * i == num:
+                    return True
+        elif 1000 < num <= 10000:
+            for i in range(30, num/30+1):
+                if i * i == num:
+                    return True
+        elif 10000 < num:
+            for i in range(100, num/100):
+                if i * i == num:
+                    return True
 
-
-#   2 - misol  ( 342 - misol)
-
-class Solution(object):
-    def isPowerOfFour(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-
-        for i in range(16):
-            if pow(4, i) == n:
-                return True
         return False
 
 
+#   2 - misol  ( 374 - misol)
 
-#   3 - misol   (344 - misol)
-
-class Solution(object):
-    def reverseString(self, s):
-        """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
-        """
-        n1 = 0
-        n2 = len(s) -1
-
-        while n1 < n2:
-            s[n1], s[n2] = s[n2], s[n1]
-            n1 += 1
-            n2 -= 1
-        return s
-
-
-
-#   4 - misol   (345 - misol)
 
 
 class Solution(object):
-    def reverseVowels(self, s):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        n = n + 1
+        m = 1
+        while True:
+
+            if (n - m) <= 2:
+                for i in range(m, n):
+                    if guess(i) == 0:
+                        return i
+
+            if guess((n + m) // 2) == 0:
+                return ((n + m) // 2)
+
+            elif guess((n + m) // 2) == -1:
+                n = ((n + m) // 2)
+
+            elif guess((n + m) // 2) == 1:
+                m = ((n + m) // 2)
+
+
+
+#   3 - misol   (383 - misol)
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        list_q = list(ransomNote)
+        list_m = list(magazine)
+
+        for i in list_q:
+            if i in list_m:
+                list_m.remove(i)
+            else:
+                return False
+        return True
+
+
+
+#   4 - misol   (387 - misol)
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
         """
         :type s: str
-        :rtype: str
+        :rtype: int
         """
-        s = list(s)
+        harf_s = {}
 
-        list1 = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        list2 = []
+        for harf in s:
+            harf_s[harf] = harf_s.get(harf, 0) + 1
 
         for i in range(len(s)):
-            if s[i] in list1:
-                list2.append(i)
+            if harf_s[s[i]] == 1:
+                return i
 
-        list3 = list2[::-1]
-
-        for i in range(len(list2) // 2):
-            s[list2[i]], s[list3[i]] = s[list3[i]], s[list2[i]]
-
-        s1 = ""
-        for i in s:
-            s1 += i
-
-        return s1
-
+        return -1
